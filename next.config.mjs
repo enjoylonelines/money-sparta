c069f8a1adcf75874c085cc7ffe9241aa8c09b2c
enjoylonelines <hobeom5022@naver.com>
2024-08-28 22:24:07 +0900

    init : pwa 초기 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
+import nextPwa from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const withPWA = nextPwa({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  // disable: process.env.NODE_ENV === 'development'
+  // customWorkerDir: "worker",
+  // runtimeCaching,
+});
+
+const config = withPWA({
+  ...nextConfig,
+});
+
+export default config;
